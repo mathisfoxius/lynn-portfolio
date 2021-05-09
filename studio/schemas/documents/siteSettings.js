8bf73a3235d5572990534320114cdce6c5684ed0
Mathis Foxius <mathis.foxius@tum.de>
2021-05-09 13:27:37 +0200

    Removed subtitles again and ran sanity graphql deploy in /studio

--- studio/schemas/documents/siteSettings.js
+++ studio/schemas/documents/siteSettings.js
@@ -15,11 +15,6 @@ export default {
       title: 'Title'
     },
     {
-      name: 'subtitle',
-      type: 'string',
-      title: 'Subtitle'
-    },
-    {
       name: 'description',
       type: 'text',
       title: 'Description',
